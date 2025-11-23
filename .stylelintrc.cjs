1bce90fcb2bdad318805e55ce187d78052cc23ce
shuainanrendekending <117963374+shuainanrendekending@users.noreply.github.com>
2025-11-23 21:52:40 +0800

    Update .stylelintrc.cjs

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -2,6 +2,7 @@ const config = require('@lobehub/lint').stylelint;
 
 module.exports = {
   ...config,
+  extends: ["stylelint-config-recess-order"],
   rules: {
     'selector-id-pattern': null,
     ...config.rules,
